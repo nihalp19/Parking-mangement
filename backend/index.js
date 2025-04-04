0889304c26e82e76afabc412c9317cb22eee206f
Nihal Panday <nihalpanday2020@gmail.com>
2025-04-04 07:04:46 +0530

    Intial commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes.js"
+import { connectDB } from "./lib/connectDB.js"
+import cors from "cors"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+
+app.use(cors({
+    origin: ["http://localhost:5173"]
+}))
+
+app.use(express.json())
+app.all("/", async (req, res) => {
+    res.send("BACKEND IS RUNNING")
+})
+
+app.use("/api",userRoutes)
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`SERVER STARTED AT PORTED NO ${PORT}`)
+})
+
